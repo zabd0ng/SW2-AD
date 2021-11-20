e3daa035cbde2ddd553d06b594216a5a7648a65e
yunmin <84099396+zabd0ng@users.noreply.github.com>
2021-11-20 15:10:23 +0900

    지하철 시간표 추출 파이썬 파일 초안, 역 ID csv 파일

---
+++ test.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+station_info = pd.read_csv('station_id.csv', encoding='CP949')
+station = input("역 이름: ")
+no = '02호선'
+station_info = station_info[station_info['전철역명']==station]
+station_info = station_info[station_info['호선']==no]
+
+station_no = station_info.iloc[0]['외부코드']
+
+url = "https://bus.go.kr/getSubway_6.jsp?statnId=1002000"+station_no+"&subwayId=1002&tabmn=5"
+response = requests.get(url)
+
+html = response.text
+soup = BeautifulSoup(html, 'html.parser')
+opportunity = soup.select_one('tr:nth-of-type(2) > td').get('rowspan')
+print(opportunity)
+uptrain = []
+downtrain = []
+arrivestationlist = soup.select('tr')[1:int(opportunity)+2]
+timelist = soup.select('tr')[3:int(opportunity)+2]
+for time in timelist:
+    t = time.get_text().replace('\n', '')
+    print(t)
