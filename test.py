bd81f5db11755d74c366070f8033c9ad7df0ff91
yunmin <84099396+zabd0ng@users.noreply.github.com>
2021-11-24 20:20:56 +0900

    1,2호선(p, -,)예외처리 적용 코드

--- test.py
+++ test.py
@@ -3,19 +3,45 @@ import requests
 from bs4 import BeautifulSoup
 
 station_info = pd.read_csv('station_id.csv', encoding='CP949')
-station = "합정"
-no = '02호선'
+station = "공덕"
+no = '06호선'
 station_info = station_info[station_info['전철역명']==station]
 station_info = station_info[station_info['호선']==no]
 
 station_no = station_info.iloc[0]['외부코드']
 
-url = "https://bus.go.kr/getSubway_6.jsp?statnId=1002000"+station_no+"&subwayId=1002&tabmn=5"
+if station == '광명':
+   url = "https://bus.go.kr/getSubway_6.jsp?statnId=1001075410&subwayId=1001&tabmn=5"
+
+elif station == '서동탄':
+   station_no = station_no.replace('P', '')
+   station_no = station_no.replace('-', '')
+   url = "https://bus.go.kr/getSubway_6.jsp?statnId=100180" + station_no + "&subwayId=1001&tabmn=5"
+
+elif 'P' in station_no:
+   station_no = station_no.replace('P', '')
+   sno = int(station_no)
+   if sno > 173:
+       sno -= 1
+       if sno > 175:
+           sno -= 1
+   url = "https://bus.go.kr/getSubway_6.jsp?statnId=1001080" + str(sno) + "&subwayId=1001&tabmn=5"
+
+
+elif '-' in station_no:
+   station_no = station_no.replace('-', '')
+   sno = int(station_no)
+   print(sno)
+   url = "https://bus.go.kr/getSubway_6.jsp?statnId=100200" + str(sno) + "&subwayId=1002&tabmn=5"
+else:
+   url = "https://bus.go.kr/getSubway_6.jsp?statnId=100"+no[1:2]+"000" + station_no + "&subwayId=100"+no[1:2]+"&tabmn=5"
+
+print(url)
 response = requests.get(url)
 
 html = response.text
 soup = BeautifulSoup(html, 'html.parser')
-# opportunity = soup.select_one('tr > td').get('rowspan') #:nth-of-type(2)
+# opportunity = soup.select_one('tr:nth-of-type(2) > td').get('rowspan')
 # print(opportunity)
 # uptrain = []
 # downtrain = []
